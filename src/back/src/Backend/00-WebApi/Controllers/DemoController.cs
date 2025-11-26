483a879c81db371648627381a2f968cf3dbd4697
Pierre-Claude N'GUESSAN <pierre-claude.nguessan@apollossc.com>
2025-11-26 18:11:37 +0100

    Fix intial config

---
+++ src/back/src/Backend/00-WebApi/Controllers/DemoController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("demo")]
+    public class DemoController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Welcome to the Enrolment CIV Docker World !";
+        }
+    }
+}

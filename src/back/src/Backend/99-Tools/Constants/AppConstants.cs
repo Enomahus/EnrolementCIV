9703a032c59f11d7c336dc38b9b0caf96e54e666
Pierre-Claude N'GUESSAN <pierre-claude.nguessan@apollossc.com>
2025-12-03 17:20:58 +0100

    Fix back tools configure for this

---
+++ src/back/src/Backend/99-Tools/Constants/AppConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace Tools.Constants
+{
+    public static class AppConstants
+    {
+        public static readonly string SuperAdminRole = "SuperAdmin";
+        public static readonly string StakeholderAdminRole = "StakeholderAdmin";
+        public static readonly string GuestRole = "GuestUser";
+    }
+}
---
+++ src/back/src/Backend/99-Tools/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace Tools.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<IEnumerable<T>> CartesianProduct<T>(
+            this IEnumerable<IEnumerable<T>> sequences
+        )
+        {
+            IEnumerable<IEnumerable<T>> emptyProduct =
+            [
+                [],
+            ];
+            return sequences.Aggregate(
+                emptyProduct,
+                (accumulator, sequence) =>
+                    from accseq in accumulator
+                    from item in sequence
+                    select accseq.Concat([item])
+            );
+        }
+    }
+}
